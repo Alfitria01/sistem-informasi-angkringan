6949f36f135e987892581c57dd553788121a7357
Al <alfitriahanifapriyadi23@mhs.politala.ac.id>
2024-10-15 10:41:19 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+// Dashboard route
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Homepage route
 Route::get('/', function () {
-    return view('welcome');
+    return view('home'); // Menampilkan homepage
 });
+
+// Resource route for products (CRUD)
+Route::resource('products', ProductController::class); // CRUD untuk produk
