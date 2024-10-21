9c53760f1e30f521fd13683c36dbf9db5ec7b9ef
Alfitria01 <alfitriahanifapriyadi23@mhs.politala.ac.id>
2024-10-21 12:41:38 +0800

    Resolve merge conflict in create_products_table migration

--- database/migrations/2024_10_14_023110_create_sales_table.php
+++ database/migrations/2024_10_14_023110_create_sales_table.php
@@ -6,19 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateSalesTable extends Migration
 {
-<<<<<<< HEAD
     /**
      * Run the migrations.
      *
      * @return void
      */
-=======
->>>>>>> bde3e209e51b67f4be84ac8811de3145ce88bef6
     public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->unsignedBigInteger('product_id'); // Add this column
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade'); // Foreign key
             $table->integer('quantity');
@@ -32,13 +28,6 @@ class CreateSalesTable extends Migration
      *
      * @return void
      */
-=======
-            $table->integer('amount');  // Kolom amount
-            $table->timestamps();  // Kolom created_at & updated_at
-        });
-    }
-
->>>>>>> bde3e209e51b67f4be84ac8811de3145ce88bef6
     public function down()
     {
         Schema::dropIfExists('sales');
